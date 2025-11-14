2501e598da7e747efc59b43259fe284a4c0a2a8d
JsMasterok <87376029+jsMasterok@users.noreply.github.com>
2025-11-14 12:27:32 +0200

    Guard vTicker init against missing element

--- header.php
+++ header.php
@@ -9,13 +9,19 @@ if (!defined('ABSPATH')) {
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php
+    $manifest_path = get_template_directory() . '/site.webmanifest';
+    if (file_exists($manifest_path)) :
+        ?>
+        <link rel="manifest" href="<?php echo esc_url(get_template_directory_uri() . '/site.webmanifest'); ?>">
+    <?php
+    endif;
+    ?>
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo esc_url(get_template_directory_uri() . '/assets/imgs/favicon.svg'); ?>">
     <?php wp_head(); ?>
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/favicon.svg">
 </head>
 
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 
-
-
-    <main class="">
+<main class="">
